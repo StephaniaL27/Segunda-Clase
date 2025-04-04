77ea2e6f1e2da4c8e5f17c097eaab5dbaf7a6133
Stephania <lstephania27@gmail.com>
2025-04-03 19:05:20 -0500

    Creamos un diccionario vacío, luego le agregamos tres valores con su clave, respectivamente y lo imprimimos

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ for i in range(3):
     a = input('¿Qué deseas agregar a la lista?')
     lista.append(a)
 print(lista)
+
+diccionario = {}
+for i in range(3):
+    a = input('¿Qué deseas agregar al diccionario?')
+    b = input('¿Qué valor deseas agregar al diccionario?')
+    diccionario[a] = b
+print(diccionario)
